e4346164351143db6f2ae990f22d82087ddadaf9
Adarsh Danda <adarsh.danda1@gmail.com>
2025-08-06 15:11:49 -0700

    added basic login and sign up page

--- apollo/src/app/login/page.tsx
+++ apollo/src/app/login/page.tsx
@@ -1,14 +1,5 @@
-import { login, signup } from '@/app/login/actions'
+import AuthContainer from "@/components/auth-container";
 
 export default function LoginPage() {
-  return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
-  )
+  return <AuthContainer logIn={true} />;
 }
---
+++ apollo/src/components/auth-form.tsx
@@ -0,0 +1,59 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import PropTypes from 'prop-types';
+import { login, signup } from '@/app/actions'
+
+
+interface AuthFormProps extends React.ComponentProps<"form"> {
+  logIn: boolean;
+}
+
+export function AuthForm({
+  className,
+  logIn,
+  ...props
+}: AuthFormProps) {
+  return (
+    <form className={cn("flex flex-col gap-6", className)} {...props}>
+      <div className="flex flex-col items-center gap-2 text-center">
+        <h1 className="text-2xl font-bold">{logIn ? 'Login to your account' : 'Sign up for an account'}</h1>
+        <p className="text-muted-foreground text-sm text-balance">
+          {logIn ? 'Enter your email below to login to your account' : 'Enter your email below to sign up for an account'}
+        </p>
+      </div>
+      <div className="grid gap-6">
+        <div className="grid gap-3">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" type="email" placeholder="m@example.com" required />
+        </div>
+        <div className="grid gap-3">
+          <div className="flex items-center">
+            <Label htmlFor="password">Password</Label>
+            <a
+              href="#"
+              className="ml-auto text-sm underline-offset-4 hover:underline"
+            >
+              Forgot your password?
+            </a>
+          </div>
+          <Input id="password" type="password" required />
+        </div>
+        <Button type="submit" className="w-full" formAction={logIn ? login : signup}>
+          {logIn ? "Login" : "Sign up"}
+        </Button>
+      </div>
+      <div className="text-center text-sm">
+        {logIn ? "Don&apos;t have an account?" : "Already have an account?"}
+        <a href="#" className="underline underline-offset-4">
+          {logIn ? "Sign up" : "Login"}
+        </a>
+      </div>
+    </form>
+  )
+}
+
+AuthForm.propTypes = {
+  logIn: PropTypes.bool.isRequired,
+}
